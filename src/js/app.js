bdc83677949f81ea3a61ff8825c47d01da9f6ec1
David promontor farias <davidgaston2341@gmail.com>
2021-07-20 17:24:57 -0500

    Se crearon todos los archivos de la carpeta src

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+
+eventListener();
+function eventListener(){
+    const cerrar = document.querySelector('.icono-cerrar');
+    const navegacion = document.querySelector('.navegacion');
+    const modoOscuro = document.querySelector('#modo-oscuro');
+    
+    cerrar.addEventListener('click', ()=>{
+        if(navegacion.classList.contains('mostrar')){
+            navegacion.classList.remove('mostrar');
+            cerrar.src = '/images/icon-hamburger.svg';
+            
+            
+        }
+        else{
+            navegacion.classList.add('mostrar');
+            cerrar.src = '/images/icon-close.svg';
+
+            
+            
+        }
+    })
+
+    modoOscuro.addEventListener('click', ()=>{
+        document.body.classList.toggle('oscuro-agregado');
+    });
+}
